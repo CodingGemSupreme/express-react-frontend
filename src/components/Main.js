16376cf826230c0d7fefef70cc11d4bc456c415b
Oma Withers <omacwitherssei@gmail.com>
2022-09-27 14:20:16 -0400

    "Updated Main Page"

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import Index from "../pages/Index";
-import Show from "../pages/Show";
+import { useEffect, useState } from "react"
+import { Route, Switch } from "react-router-dom"
+import Index from "../pages/Index"
+import Show from "../pages/Show"
 
 function Main(props) {
   const [ people, setPeople ] = useState(null);
@@ -12,9 +12,9 @@ function Main(props) {
     const response = await fetch(URL);
     const data = await response.json();
     setPeople(data);
-  };
+  }
 
-  const createPeople = async (person) => {
+  const createPeople = async person => {
     // make post request to create people
     await fetch(URL, {
       method: "POST",
@@ -25,7 +25,29 @@ function Main(props) {
     });
     // update list of people
     getPeople();
-  };
+  }
+
+  const updatePeople = async (person, id) => {
+    // make put request to create people
+    await fetch(URL + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    // update list of people
+    getPeople();
+  }
+
+  const deletePeople = async id => {
+    // make delete request to create people
+    await fetch(URL + id, {
+      method: "DELETE",
+    })
+    // update list of people
+    getPeople();
+  }
 
   useEffect(() => getPeople(), []);
 
@@ -37,8 +59,11 @@ function Main(props) {
         </Route>
         <Route
           path="/people/:id"
-          render={(rp) => (
+          render={rp => (
             <Show
+              people={people}
+              updatePeople={updatePeople}
+              deletePeople={deletePeople}
               {...rp}
             />
           )}
